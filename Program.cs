76dc8600cdc54f6d9dc7050f0ff1b83ba9aa35e9
oruchankural <oruchankural@gmail.com>
2023-12-08 02:14:18 +0300

    Initial Committ

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium.Chrome;
+var chromeOptions = new ChromeOptions();
+chromeOptions.AddArgument("--headless");
+
+using (var driver = new ChromeDriver(chromeOptions))
+{
+    driver.Navigate().GoToUrl("https://demo3.kodzip.com/");
+    Console.WriteLine("Page Title: " + driver.Title);
+}
